ce03724d19df0f0c9144c66736655b6cef591d05
abhi587 <abhishek.pawan14@gmail.com>
2026-05-31 20:27:51 +0530

    ErrorHandelingChanged

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 const express = require("express");
-const healthRoutes = require("./routes/healthRoutes");
-
-const app = express();
-
-app.use(express.json());
-
-app.use("/api", healthRoutes);
-
-module.exports = app;const express = require("express");
 
 const healthRoutes = require("./routes/healthRoutes");
 const authRoutes = require("./routes/authRoutes");
+// const userRoutes = require("./routes/userRoutes");
+// const projectRoutes = require("./routes/projectRoutes");
+// const taskRoutes = require("./routes/taskRoutes");
 const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
@@ -19,6 +13,9 @@ app.use(express.json());
 
 app.use("/api", healthRoutes);
 app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/projects", projectRoutes);
+// app.use("/api/tasks", taskRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
